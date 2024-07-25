8e8c348376d12f149c2d141bb470376212596fcf
kaanaldemir <kaanaldemir1608@gmail.com>
2024-07-25 14:49:10 +0300

    Added Splash Screen

---
+++ app/src/main/java/com/mipo/mipsos/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.mipo.mipsos
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Start MainActivity
+            startActivity(Intent(this, MainActivity::class.java))
+            // Finish SplashActivity
+            finish()
+        }, 3000) // 3 seconds delay
+    }
+}
